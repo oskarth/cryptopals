fc285a7077e72c2f6054ac9dd833af09ca04b1ef
Oskar Thoren <ot@oskarthoren.com>
2019-06-24 08:35:02 +0800

    fixup fixedxor, take byte strings
    also assert

--- set1/fixedxor.nim
+++ set1/fixedxor.nim
@@ -1,10 +1,9 @@
 import strutils
 
-proc fixedxor*(a: string, b: string): string =
-    assert len(a) == len(b)
+# NOTE: Maybe declare separate bytestring type?
+proc fixedxor*(bs1: string, bs2: string): string =
+    assert(len(bs1) == len(bs2), "Not equal length")
     var res: string
-    let bs1 = parseHexStr(a)
-    let bs2 = parseHexStr(b)
 
     for i in countup(0, len(bs1)-1):
         res.add(char((ord(bs1[i]) xor ord(bs2[i]))))
@@ -16,5 +15,7 @@ const a = "1c0111001f010100061a024b53535009181c"
 const b = "686974207468652062756c6c277320657965"
 const c = "746865206b696420646f6e277420706c6179"
 
-echo fixedxor(a, b)
-assert (fixedxor(a, b) == parseHexStr(c))
+let bs1 = parseHexStr(a)
+let bs2 = parseHexStr(b)
+
+assert (fixedxor(bs1, bs2) == parseHexStr(c))
