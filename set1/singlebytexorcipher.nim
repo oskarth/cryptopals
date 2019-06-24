42d4f0645030366da32bf519b15cff308ccf3254
Oskar Thoren <ot@oskarthoren.com>
2019-06-24 09:19:00 +0800

    1.3. Tweak signature to return CipherResult
    Expose too

--- set1/singlebytexorcipher.nim
+++ set1/singlebytexorcipher.nim
@@ -4,7 +4,13 @@ import fixedxor
 const input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 const common_chars_set = {' ', 'e', 't', 'a', 'o', 'i'}
 
-proc xorcipher(input: string, iord: int): string =
+type
+  CipherResult* = object
+    text*: string
+    score*: int
+    character*: char
+
+proc xorcipher*(input: string, iord: int): string =
     let bs1 = parseHexStr(input)
     let character = char(iord)
     var repeated_character: string
@@ -20,16 +26,14 @@ proc score(input: string): int =
             score += 1
     return score
 
-proc findbest() =
-    var best_score = 0
-    var best: string
+proc findbest*(input: string): CipherResult =
+    var best = CipherResult(text: "", score: 0, character: '0')
     for i in countup(0, 127):
         let text = xorcipher(input, i)
         let text_score = score(text)
-        if text_score > best_score:
-            best = text
-            best_score = text_score
-    echo "Best score found: ", best_score
-    echo best
+        if text_score > best.score:
+            best.text = text
+            best.score = text_score
+    return best
 
-findbest()
+#echo findbest(input)
