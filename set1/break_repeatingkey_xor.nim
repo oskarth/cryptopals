8679715d6c185d42e8de4a50e5e819b0c30e4041
Oskar Thoren <ot@oskarthoren.com>
2019-06-24 12:57:30 +0800

    1.6. Guess key size and transpose blocks

--- set1/break_repeatingkey_xor.nim
+++ set1/break_repeatingkey_xor.nim
@@ -1,8 +1,7 @@
 import strutils
 import tables
 import fixedxor
-
-var keysize = 2 # vary upto 40
+import base64
 
 const ex1 = "this is a test"
 const ex2 = "wokka wokka!!!"
@@ -27,3 +26,59 @@ proc distance(a: string, b:string): int =
     return dist
 
 assert(distance("this is a test", "wokka wokka!!!") == 37)
+
+# Let's open file and turn diff
+
+var encrypted_hex = ""
+var f = open("6.txt")
+for line in f.lines:
+    #echo toHex(decode(line))
+    encrypted_hex = decode(line)
+f.close()
+
+
+# # XXX: Only doing keysize up to 20, since we assume we are repeating at least once
+# for keysize in countup(2, 20):
+#     let dist = distance(encrypted_hex[0..keysize-1], encrypted_hex[keysize..keysize+keysize-1])
+#     let normalized = dist / keysize
+#     echo "keysize=", keysize, ": ", normalized
+
+# This points to
+# keysize=11: 2.636363636363636
+# But keysize=12: 2.833333333333333
+# Also a candidate, try with 4 blocks?
+# keysize=18: 2.944444444444445
+# Let's assume keysize=11 for now
+
+# I'm not sure I follow edit distance relevance here though, bits differ less between blocks
+# because there's some common information in them? something like this
+
+# XXX: Only doing keysize up to 20, since we assume we are repeating at least once
+proc guess_keysize(text: string): int =
+    var bestguess_dist = 999.9
+    var bestguess = 0
+
+    for keysize in countup(2, 20):
+        let dist = distance(text[0..keysize-1], text[keysize..keysize+keysize-1])
+        let normalized = dist / keysize
+        if normalized < bestguess_dist:
+            bestguess_dist = normalized
+            bestguess = keysize
+
+    return bestguess
+
+#echo guess_keysize(encrypted_hex)
+
+# Assuming keysize=11
+var blocks: seq[string]
+for i in countup(0,10):
+    var bl = ""
+    var index = i
+    while index <= len(encrypted_hex)-1:
+        bl = bl & encrypted_hex[index]
+        index += 10
+
+    blocks.add(bl)
+
+for b in blocks:
+    echo toHex(b)
