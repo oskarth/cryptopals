f15f7c3fb216232728d5f8527383302626f728ea
Oskar Thoren <ot@oskarthoren.com>
2019-06-25 10:10:10 +0800

    Repeated xorcipher do reverse op sanity check

--- set1/repeatingkey_xor.nim
+++ set1/repeatingkey_xor.nim
@@ -1,5 +1,6 @@
 import strutils
 import fixedxor
+import base64
 
 const input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 const key = "ICE"
@@ -11,5 +12,9 @@ proc repeated_xorcipher*(bs1: string, key: string): string =
     fixedxor(bs1, repeated_character)
 
 #echo toHex(repeated_xorcipher(input, key))
+
+# Go from encrypted hex to cleartext
+let encrypted_hex = "0B3637272A2B2E63622C2E69692A23693A2A3C6324202D623D63343C2A26226324272765272A282B2F20430A652E2C652A3124333A653E2B2027630C692B20283165286326302E27282F"
+#echo repeated_xorcipher(parseHexStr(decode(encode(encrypted_hex))), "ICE")
 
 # Extension: read from file as argument / standard input
