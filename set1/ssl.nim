43d6f8666e5cc6a594b8220a0a347c639cbf9161
Oskar Thoren <ot@oskarthoren.com>
2019-06-26 11:12:46 +0800

    Test custom SHA1 OpenSSL binding

--- set1/ssl.nim
+++ set1/ssl.nim
@@ -1,7 +1,21 @@
-from strutils import toHex
+# Compiles and runs with:
+# nim c -d:ssl -r ssl.nim
+
+import strutils
 from openssl import getOpenSSLVersion
 
 echo "version: 0x" & $getOpenSSLVersion().toHex()
 
-# Compiles and runs with:
-# nim c -d:ssl -r ssl.nim
+const SHA1Len = 20
+
+# XXX: Hacky, specifying version number manually to find shared library
+# Corresponds to https://github.com/openssl/openssl/blob/master/include/openssl/sha.h#L44
+proc SHA1(d: cstring, n: culong, md: cstring = nil): cstring {.cdecl, dynlib: "libssl.so.1.1", importc.}
+
+proc SHA1(s: string): string =
+    result = ""
+    var s = SHA1(s.cstring, s.len.culong)
+    for i in countup(0, SHA1Len-1):
+        result.add s[i].BiggestInt.toHex(2).toLower
+
+echo SHA1("Rosetta Code")
